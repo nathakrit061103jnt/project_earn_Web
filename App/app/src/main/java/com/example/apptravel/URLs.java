5a0ef781e1ffc80f94346dfb2ef500cd6135682e
nathakrit061103jnt <std60010912540@acc.msu.ac.th>
2021-04-04 16:16:25 +0700

    update

--- App/app/src/main/java/com/example/apptravel/URLs.java
+++ App/app/src/main/java/com/example/apptravel/URLs.java
@@ -1,7 +1,7 @@
 package com.example.apptravel;
 
 public class URLs {
-    public static final String IP = "http://192.168.1.28:81/";
+    public static final String IP = "http://192.168.1.28/";
     public static final String ROOT_URL = IP + "project_e/android_api/";
     public static final String URL_REGISTER = ROOT_URL + "register.php";
     public static final String URL_LOGIN = ROOT_URL + "login.php";
